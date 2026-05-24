f8567df0451cf8dcf1187c012f12cb5bd782ec4e
gr00vytony <chris@withstell.com>
2026-05-24 16:35:01 -0400

    Use arrow icons in Contact reach card

--- wp-content/themes/rocketpd/template-parts/pages/contact/hero.php
+++ wp-content/themes/rocketpd/template-parts/pages/contact/hero.php
@@ -107,6 +107,9 @@ $quick_items = array(
 						<span class="rpd-contact-quick__label"><?php echo esc_html( $item['label'] ); ?></span>
 						<span class="rpd-contact-quick__value"><?php echo esc_html( $item['value'] ); ?></span>
 					</span>
+					<span class="rpd-contact-quick__arrow" aria-hidden="true">
+						<svg viewBox="0 0 24 24" focusable="false"><path d="M5 12h14"/><path d="m13 6 6 6-6 6"/></svg>
+					</span>
 				</a>
 			<?php endforeach; ?>
 		</aside>
