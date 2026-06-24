711f026d82049be36fa36ef094965d7473ea4a40
Joefer Traya <125757323+jNsatiable@users.noreply.github.com>
2026-06-25 07:42:48 +0800

    Revert "Merge pull request #38 from therocketpdteam/feat/courses-section-modes"
    This reverts commit c1b8660789184a13b30ebbf228c22d5f3408f695, reversing
    changes made to 339d81c85cde1b07e592285ffb7e69015422796f.

--- wp-content/themes/rocketpd/page-templates/template-courses.php
+++ wp-content/themes/rocketpd/page-templates/template-courses.php
@@ -14,30 +14,14 @@ get_header();
 
 while ( have_posts() ) {
 	the_post();
-
-	$hero_mode    = rocketpd_get_field( 'rpd_courses_hero_mode', 'defaults' );
-	$formats_mode = rocketpd_get_field( 'rpd_courses_formats_mode', 'defaults' );
-	$gallery_mode = rocketpd_get_field( 'rpd_courses_gallery_mode', 'defaults' );
-	$guide_mode   = rocketpd_get_field( 'rpd_courses_guide_mode', 'defaults' );
-	$cta_mode     = rocketpd_get_field( 'rpd_courses_cta_mode', 'defaults' );
 	?>
 	<main id="primary" class="rpd-site-main rpd-courses-page">
 		<?php
-		if ( 'hidden' !== $hero_mode ) {
-			get_template_part( 'template-parts/pages/courses/hero' );
-		}
-		if ( 'hidden' !== $formats_mode ) {
-			get_template_part( 'template-parts/pages/courses/formats' );
-		}
-		if ( 'hidden' !== $gallery_mode ) {
-			get_template_part( 'template-parts/pages/courses/gallery' );
-		}
-		if ( 'hidden' !== $guide_mode ) {
-			get_template_part( 'template-parts/pages/courses/decision-guide' );
-		}
-		if ( 'hidden' !== $cta_mode ) {
-			get_template_part( 'template-parts/pages/courses/district-cta' );
-		}
+		get_template_part( 'template-parts/pages/courses/hero' );
+		get_template_part( 'template-parts/pages/courses/formats' );
+		get_template_part( 'template-parts/pages/courses/gallery' );
+		get_template_part( 'template-parts/pages/courses/decision-guide' );
+		get_template_part( 'template-parts/pages/courses/district-cta' );
 		?>
 	</main>
 	<?php
